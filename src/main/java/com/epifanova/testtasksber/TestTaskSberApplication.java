cba3478074dfa495042a70bc7a77c554cf9938e1
Valeria Epifanova <epifanova.valera@yandex.ru>
2023-09-21 14:40:45 +0300

    Initial commit

---
+++ src/main/java/com/epifanova/testtasksber/TestTaskSberApplication.java
@@ -0,0 +1,13 @@
+package com.epifanova.testtasksber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskSberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskSberApplication.class, args);
+	}
+
+}
